e57c8490373efbda5a23efa06bf5eed91b2fe992
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-09-11 15:53:40 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Swarm
+
+Swarm is a tool for running locust in a distributed fashion on a set of load generator slaves. It also has basic support for jmeter.
+
+Swarm supports [locust-extensions](https://github.com/SvenskaSpel/locust-extensions), allowing you to do things like RPS limiting in a distributed setting.
+
+## Installation
+
+```pip install locust-swarm```
+
+Swarm uses SSH to launch remote processes and SSH tunnels for communication, so your first step should be to ensure you can access the slaves over ssh.
+
+## Detailed help
+
+```swarm -h```
+
+## License
+
+Copyright 2018 AB SvenskaSpel
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ast
+import re
+
+from setuptools import find_packages, setup
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+_init_file = "locust_swarm/__init__.py"
+with open(_init_file, "rb") as f:
+    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
+
+setup(
+    name="locust-swarm",
+    version=version,
+    description="Load test distribution tool for Locust (and JMeter)",
+    long_description="""""",
+    classifiers=[
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+    ],
+    keywords="",
+    author="Lars Holmberg",
+    author_email="lars.holmberg@svenskaspel.se",
+    url="https://github.com/SvenskaSpel/locust-swarm",
+    license="Apache-2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "locustio",
+        "locust-extensions@git+ssh://git@git.svenskaspel.se/test/locust-extensions",
+        "psutil",
+    ],
+    entry_points={"console_scripts": ["swarm = locust_swarm.main:main"]},
+)
