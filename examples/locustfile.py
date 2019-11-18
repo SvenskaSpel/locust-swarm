a976b338b72b78b1e1f8dd4aa7a57f9b12d584a0
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-11-18 16:21:16 +0100

    Simplify example (remove debugging, switch to fasthttplocust, remove PostgresReader)

--- examples/locustfile.py
+++ examples/locustfile.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python3
 import os
-import locust_plugins.utils
 
-locust_plugins.utils.gevent_debugger_patch()
-locust_plugins.utils.print_json_on_decode_fail()
-from locust_plugins.listeners import PrintListener, TimescaleListener
-from locust_plugins.readers import PostgresReader
+from locust_plugins.listeners import TimescaleListener
 from locust_plugins.tasksets import TaskSetRPS
-from locust import HttpLocust, task
+from locust.contrib.fasthttp import FastHttpLocust
+from locust import task
+from locust.wait_time import constant
 
-TimescaleListener("example")
+TimescaleListener("example", os.environ["LOCUST_TEST_ENV"])
 
-CUSTOMER_READER = PostgresReader(os.environ["LOCUST_TEST_ENV"])
 RPS = float(os.environ["LOCUST_RPS"])
 
 
@@ -20,20 +16,8 @@ class UserBehavior(TaskSetRPS):
     def my_task(self):
         self.rps_sleep(RPS)
         self.client.get("/")
-        customer = CUSTOMER_READER.get()
-        self.client.post("/", data={"ssn": customer["ssn"]})
-        CUSTOMER_READER.release(customer)
 
 
-class WebsiteUser(HttpLocust):
+class WebsiteUser(FastHttpLocust):
     task_set = UserBehavior
-    min_wait = 0
-    max_wait = 0
-    if __name__ == "__main__":
-        host = "http://example.com"
-
-
-# allow running as executable, mainly to support attaching the debugger
-if __name__ == "__main__":
-    PrintListener()
-    WebsiteUser().run()
+    wait_time = constant(0)
