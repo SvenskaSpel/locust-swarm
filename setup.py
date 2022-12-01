cf19ed0237deba34d1f68efe40a35b509d86794b
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-12-01 11:09:15 +0100

    Remove strict dependency on locust-plugins, but require a compatible version of locust-plugins if (and only if) it is actuall installed.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+import importlib.util
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 from setuptools.command.egg_info import egg_info
@@ -29,6 +30,16 @@ class PostEggInfoCommand(egg_info):
             sys.exit("Looks like you are on windows. Only MacOS and Linux are supported :(")
         egg_info.run(self)
 
+
+requirements_list = [
+    "keyring==21.4.0",
+    "psutil",
+    "ConfigArgParse>=1.0",
+]
+# if locust-plugins IS installed, then require a version known to work with this version of swarm.
+spec = importlib.util.find_spec("locust_plugins")
+if spec is not None:
+    requirements_list.append("locust-plugins>=2.7.0")
 
 setup(
     name="locust-swarm",
@@ -57,12 +68,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        "keyring==21.4.0",
-        "locust-plugins>=2.2.2",
-        "psutil",
-        "ConfigArgParse>=1.0",
-    ],
+    install_requires=requirements_list,
     entry_points={
         "console_scripts": ["swarm = locust_swarm.swarm:main"],
     },
